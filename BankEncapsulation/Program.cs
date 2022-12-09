68d70941deda51f56f4e27be17e0c971024367e6
Devin1824 <devster18@gmail.com>
2022-12-09 08:20:35 -0800

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount myBank = new BankAccount();
+            bool check = true;
+
+
+
+            do
+            {
+                Console.Write("Enter your pin # : ");
+                int pin = int.Parse(Console.ReadLine());
+                bool verify = myBank.GetPin(pin);
+                if (verify == true)
+                {
+                    Console.Write("Choose amount to deposit: ");
+                    double amount = double.Parse(Console.ReadLine());
+                    myBank.Deposit(amount);
+                    double balance = myBank.GetBalance();
+                    Console.WriteLine($"Your Bank Account Balance is ${balance}");
+                    check = true;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect pin, Try again.");
+                    check = false;
+                }
+
+            } while (!check);
+
+
+
+
+
+
+
+
+
         }
     }
 }
